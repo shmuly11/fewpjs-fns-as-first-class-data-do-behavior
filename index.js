62c80ed5b4822de69e279d1878e056f192be8036
shmuly11 <shmuly11@gmail.com>
2021-01-31 15:24:30 -0500

    commit tree fb979eb13a914b7edcd3030098c49ff9f9349d6b

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(input){
+  let time = input.split(":");
+  let hour = parseInt(time[0]);
+  if (hour < 12) {
+    return "Good Morning"
+  } else if(hour >= 12 && hour < 18){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(input){
+  document.getElementById('greeting').innerText = input
+}
